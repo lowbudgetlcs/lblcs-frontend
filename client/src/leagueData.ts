dbbe7f2f812a0eb65173a308459c7ece9ca0edef
Mamii868 <marcmotenii@gmail.com>
2024-10-07 12:14:04 -0500

    Add stat and performance data fetch

--- client/src/leagueData.ts
+++ client/src/leagueData.ts
@@ -27,35 +27,80 @@ export interface DivisionProps {
   groups: number;
 }
 
+export interface StatProps {
+    id: number;
+    kills: number;
+    deaths: number;
+    assists: number;
+    level: number;
+    gold: number;
+    visionScore: number;
+    damage: number;
+    healing: number;
+    shielding: number;
+    damageTaken: number;
+    selfMitigatedDamage: number;
+    damageToTurrets: number;
+    longestLife: number;
+    doubleKills: number;
+    tripleKills: number;
+    quadraKills: number;
+    pentaKills: number;
+    gameLength: number;
+    win: number;
+    cs: number;
+    championName: Text;
+    teamKills: number;
+    shortCode: Text;
+    performanceId: number | null;
+
+}
+
+export interface PerformanceProps {
+  id: number;
+  teamId: number | null;
+  playerId: number | null;
+  divisionId: number | null;
+  gameId: number | null;
+}
+
 export const useFetchData = () => {
   const [players, setPlayers] = useState<PlayerProps[]>([]);
   const [teams, setTeams] = useState<TeamProps[]>([]);
   const [divisions, setDivisions] = useState<DivisionProps[]>([]);
+  const [stats, setStats] = useState<StatProps[]>([]);
+  const [performance, setPerformance] = useState<PerformanceProps[]>([])
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [playersResponse, teamsResponse, divisionsResponse] = await Promise.all([
-          fetch("https://backend.lowbudgetlcs.com/api/getPlayers"),
-          fetch("https://backend.lowbudgetlcs.com/api/getTeams"),
-          fetch("https://backend.lowbudgetlcs.com/api/getDivisions"),
+        const [playersResponse, teamsResponse, divisionsResponse, statsResponse, performancesResponse] = await Promise.all([
+          fetch(import.meta.env.VITE_PLAYERS_URL as string),
+          fetch(import.meta.env.VITE_TEAMS_URL as string),
+          fetch(import.meta.env.VITE_DIVISIONS_URL as string),
+          fetch(import.meta.env.VITE_STATS_URL as string),
+          fetch(import.meta.env.VITE_PERFORMANCES_URL as string),
         ]);
 
-        if (!playersResponse.ok || !teamsResponse.ok || !divisionsResponse.ok) {
+        if (!playersResponse.ok || !teamsResponse.ok || !divisionsResponse.ok || !statsResponse.ok || !performancesResponse.ok) {
           throw new Error("Failed to fetch data");
         }
 
-        const [playersData, teamsData, divisionsData] = await Promise.all([
+        const [playersData, teamsData, divisionsData, statsData, performanceData] = await Promise.all([
           playersResponse.json() as Promise<PlayerProps[]>,
           teamsResponse.json() as Promise<TeamProps[]>,
           divisionsResponse.json() as Promise<DivisionProps[]>,
+          statsResponse.json() as Promise<StatProps[]>,
+          performancesResponse.json(), 
         ]);
 
         setPlayers(playersData);
         setTeams(teamsData);
         setDivisions(divisionsData);
+        setStats(statsData);
+        setPerformance(performanceData);
       } catch (err) {
         const error = err as FetchError;
         setError(error.message);
@@ -67,5 +112,5 @@ export const useFetchData = () => {
     fetchData();
   }, []);
 
-  return { players, teams, divisions, error, loading };
+  return { players, teams, divisions, stats, performance, error, loading };
 };
